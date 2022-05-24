2f227cd2ee06ad3ee937245383eccfb74b9ec5b2
Zeyu Mao <mao.mzy@gmail.com>
2022-05-23 23:16:40 -0500

    1. FastAPI is fully working now.

--- server/app.py
+++ server/app.py
@@ -17,9 +17,6 @@ mimetypes.add_type('text/css', '.css')
 
 user_script = importlib.import_module(
     sys.argv[1])  # Dynamically import user script
-# app = flask.Flask(__name__, static_url_path="/static")
-# app.config['JSON_SORT_KEYS'] = False
-# sock = flask_sock.Sock(app)
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -34,94 +31,50 @@ async def init():
         "state": initial_state,
         "components": active_components
     }
-    print(response_payload)
-    response = json.dumps(response_payload, default=lambda x: True)
 
-    return response
+    return response_payload
 
 
 # Listen to events. Keep the WebSockets stream open and waiting for forwarded events from the frontend.
 # Call the event handlers. These will likely modify session state.
 # Respond with state mutations and active components.
 
-# @sock.route("/api/stream")
-# def stream(sock):
 
-#     # Each session gets its own state, initialised with the contents of the initial state.
+@app.websocket("/api/stream")
+async def stream(websocket: WebSocket):
+    await websocket.accept()
+    session_state = copy.deepcopy(user_script.ss.initial_state)
+    while True:
+        data = await websocket.receive_json()
+        type = data["type"]
+        target_id = data["targetId"]
+        value = data["value"]
 
-#     session_state = copy.deepcopy(user_script.ss.initial_state)
-#     while True:
-#         data = json.loads(sock.receive())
-#         type = data["type"]
-#         target_id = data["targetId"]
-#         value = data["value"]
+        # Get active components. That is, components that don't depend on a conditioner (conditional rendering function)
+        # or components for which their conditioner returns True.
 
-#         # Get active components. That is, components that don't depend on a conditioner (conditional rendering function)
-#         # or components for which their conditioner returns True.
+        session_components = user_script.ss.get_active_components(
+            session_state)
 
-#         session_components = user_script.ss.get_active_components(session_state)
+        # Trigger handler (component needs to be active in the session)
 
-#         # Trigger handler (component needs to be active in the session)
+        session_components[target_id]["handlers"][type](session_state, value)
 
-#         session_components[target_id]["handlers"][type](session_state, value)
+        # Reobtaining session components to account for state changes that may have caused components to become active/inactive
 
-#         # Reobtaining session components to account for state changes that may have caused components to become active/inactive
+        session_components = user_script.ss.get_active_components(
+            session_state)
 
-#         session_components = user_script.ss.get_active_components(session_state)
+        msg = json.dumps({
+            "mutations": session_state.mutations(),
+            "components": session_components
+        }, default=lambda x: True)
 
-#         sock.send(json.dumps({
-#             "mutations": session_state.mutations(),
-#             "components": session_components
-#         }, default=lambda x: True))
-
-
-# @app.websocket("/api/stream")
-# async def stream(websocket: WebSocket):
-#     await websocket.accept()
-#     session_state = copy.deepcopy(user_script.ss.initial_state)
-#     while True:
-#         data = await json.loads(websocket.receive_json())
-#         type = data["type"]
-#         target_id = data["targetId"]
-#         value = data["value"]
-
-#         # Get active components. That is, components that don't depend on a conditioner (conditional rendering function)
-#         # or components for which their conditioner returns True.
-
-#         session_components = user_script.ss.get_active_components(session_state)
-
-#         # Trigger handler (component needs to be active in the session)
-
-#         session_components[target_id]["handlers"][type](session_state, value)
-
-#         # Reobtaining session components to account for state changes that may have caused components to become active/inactive
-
-#         session_components = user_script.ss.get_active_components(session_state)
-
-#         msg = json.dumps({
-#             "mutations": session_state.mutations(),
-#             "components": session_components
-#         }, default=lambda x: True)
-
-#         await websocket.send_json(msg)
+        await websocket.send_text(msg)
 
 
 # Serve static files
 
-# @app.get('/', defaults={'path': ''})
-# async def index(path):
-#     return app.send_static_file('index.html')
-
-# templates = Jinja2Templates(directory="static")
-
-
-# @app.get("/")
-# async def send_static(request: Request):
-#     # directory = app.root_path + "/static"
-#     # return flask.send_from_directory(directory=directory, path=path)
-#     return templates.TemplateResponse(
-#         "index.html", {"request": request}
-#     )
 @app.get("/")
 async def read_index():
     return FileResponse('static/index.html')
@@ -134,8 +87,7 @@ async def read_index(request: Request):
     file = "static/" + path
     return FileResponse(file)
 
-# Start Flask
+# Start FastAPI
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0')
     uvicorn.run(app, host="0.0.0.0", port=5000)
