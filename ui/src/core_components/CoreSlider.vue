b2391b63bbbc5e4c8235d62c1d318eb5f34aae15
Zeyu Mao <mao.mzy@gmail.com>
2022-05-27 16:46:36 -0500

    1. Replace slider with standard ui component.

--- ui/src/core_components/CoreSlider.vue
+++ ui/src/core_components/CoreSlider.vue
@@ -1,103 +1,103 @@
 <template>
-	<div class="CoreSlider component" :data-streamsync-id="componentId" v-show="!isPlaceholder">
-        
-        <div class="main">
-            <div class="inputContainer">
-                <input type="range" :min="min" :max="max" :value="value" v-on:change.prevent="handleChange($event)" />
-            </div>
-            
-            <div class="rangeLabelContainer">
-                <div class="min">{{ min }}</div>
-                <div class="max">{{ max }}</div>
-            </div>
-        </div>
-
-        <div class="label">
-            <h2>{{ value }}</h2>
-        </div>
-    </div>
+  <n-config-provider :theme="theme.theme">
+    <n-slider
+      :min="min"
+      :max="max"
+      v-model="value"
+      @update:value="handleChange($event)"
+      :data-streamsync-id="componentId"
+      v-show="!isPlaceholder"
+    />
+  </n-config-provider>
 </template>
 
 <script>
+import { useThemeStore } from "@/stores/theme";
+
 export default {
-    inject: ["streamsync"],
-    emit: ["change"],
-    data: function () {
-        return {
-            value: 0
-        }
+  inject: ["streamsync"],
+  setup() {
+    const theme = useThemeStore();
+    return {
+      theme,
+    };
+  },
+  //   emit: ["change"],
+  data: function () {
+    return {
+      value: 0,
+    };
+  },
+  props: {
+    componentId: String,
+  },
+  mounted: function () {
+    this.streamsync.addEventListeners(this.componentId, this.$el);
+  },
+  methods: {
+    handleChange: function (value) {
+      let event = {};
+      event.type = "change";
+      event.targetId = this.componentId;
+      event.value = value;
+      this.streamsync.forward(event, true);
     },
-	props: {
-        componentId: String
+  },
+  computed: {
+    text: function () {
+      return this.streamsync.getContentValue(this.componentId, "text");
     },
-    mounted: function () {
-        this.streamsync.addEventListeners(this.componentId, this.$el);
+    min: function () {
+      const v = this.streamsync.getContentValue(this.componentId, "min");
+      if (v === null) return 0;
+      return v;
     },
-    methods: {
-        handleChange: function (ev) {
-            this.value = ev.target.value;
-            console.log(ev)
-            console.log(typeof ev)
-            this.$emit("change", ev);
-        }
+    max: function () {
+      const v = this.streamsync.getContentValue(this.componentId, "max");
+      if (v === null) return 0;
+      return v;
     },
-    computed: {
-        text: function () {
-            return this.streamsync.getContentValue(this.componentId, "text");
-        },
-        min: function () {
-            const v = this.streamsync.getContentValue(this.componentId, "min");
-            if (v === null) return 0;
-            return v;
-        },
-        max: function () {
-            const v = this.streamsync.getContentValue(this.componentId, "max");
-            if (v === null) return 0;
-            return v;
-        },
-        isPlaceholder: function () {
-            return this.streamsync.components[this.componentId].placeholder;
-        }
-    }
-}
+    isPlaceholder: function () {
+      return this.streamsync.components[this.componentId].placeholder;
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 .CoreSlider {
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 }
 
 .main {
-    display: block;
-    padding-top: 0.7rem;
-    width: 30ch;
+  display: block;
+  padding-top: 0.7rem;
+  width: 30ch;
 }
 
 input {
-    width: 100%;
-    margin: 0;
+  width: 100%;
+  margin: 0;
 }
 
 .rangeLabelContainer {
-    align-items: center;
-    display: flex;
-    justify-content: space-between;
-    font-size: 0.7rem;
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  font-size: 0.7rem;
 }
 
 h2 {
-    margin: 0;
+  margin: 0;
 }
 
 .label {
-    min-width: 32px;
-    text-align: center;
-    margin-left: 24px;
-    padding: 16px;
-    border-radius: 8px;
-    background: var(--subtleHighlight);
+  min-width: 32px;
+  text-align: center;
+  margin-left: 24px;
+  padding: 16px;
+  border-radius: 8px;
+  background: var(--subtleHighlight);
 }
-
 </style>
